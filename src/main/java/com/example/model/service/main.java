8939e91deb113b27d6ba83e0896783ef8b738eb5
Vinaykumarreddy467 <vinaykumarreddy467@gmail.com>
2024-09-25 20:31:45 -0700

    Updated with recent changes

--- src/main/java/com/example/model/service/main.java
+++
@@ -1,8 +0,0 @@
-package com.example.model.service;
-
-
-public class main {
-	
-	
-
-}
--- src/main/java/com/model/WebappApplication.java
+++ src/main/java/com/model/WebappApplication.java
@@ -3,14 +3,12 @@ package com.model;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.OnlineBanking;
-
 @SpringBootApplication
 public class WebappApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebappApplication.class, args);
-        
+        System.out.println("hi");
         System.setProperty("server.servlet.context-path", "/banking");
-        SpringApplication.run(.class, args);
+        SpringApplication.run(WebappApplication.class, args);
     }
 }
